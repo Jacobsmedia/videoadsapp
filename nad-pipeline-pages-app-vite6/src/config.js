b74f558c04a236a7ea8fa7a07d5a313fef1db548
Jacobsmedia <jacobsmedia12@gmail.com>
2026-05-20 14:07:04 +0200

    Delete nad-pipeline-pages-app-vite6 directory

--- nad-pipeline-pages-app-vite6/src/config.js
+++
@@ -1,6 +0,0 @@
-const DEFAULT_PROXY_URL = "https://kie-proxy.jacobsmedia12.workers.dev";
-
-export function getProxyUrl() {
-  const configured = import.meta.env.VITE_KIE_PROXY_URL || DEFAULT_PROXY_URL;
-  return configured.replace(/\/+$/, "");
-}
